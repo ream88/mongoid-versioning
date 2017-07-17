65e02deac0c06911666f459bf7fe1e7cc1e2d800
Ingus Skaistkalns <ingus.skaistkalns@makit.lv>
2017-07-17 08:58:02 +0300

    Change options validation method instead of altering constant

--- lib/mongoid/core_ext/relations/options.rb
+++ lib/mongoid/core_ext/relations/options.rb
@@ -1,7 +1,21 @@
 module Mongoid
   module Relations
     module Options
-      COMMON << :versioned
+      VERSIONED_OPTIONS  = [:versioned].freeze
+
+      def validate!(options)
+        valid_options = options[:relation]::VALID_OPTIONS + COMMON + VERSIONED_OPTIONS
+        options.keys.each do |key|
+          if !valid_options.include?(key)
+            raise Errors::InvalidOptions.new(
+              options[:name],
+              key,
+              valid_options
+            )
+          end
+        end
+        true
+      end
     end
   end
 end
